56d4f0ebdbf53931936988e5725f28391518d861
shivam <rajshivam691@gmail.com>
2022-09-22 12:26:00 -0700

    login change

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+
+import 'login_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:AnimatedSplashScreen(
+        splash: Stack(
+           children: [
+            Center(
+                child:Container(
+                  child:Image.asset('images/app_logo.png'),
+                )
+            )
+          ],
+        ),
+        nextScreen: const LoginScreen(),
+        splashTransition: SplashTransition.rotationTransition,
+        pageTransitionType: PageTransitionType.leftToRightWithFade,
+        backgroundColor: const Color(0xffF5591F),
+        duration: 300,
+      ),
+    );
+  }
+}
+
+
